5801277ca41215034cbac37e073cfd9135f8a828
Thomas Perez <thomas@scullwm.com>
2015-04-17 13:42:51 +0200

    use MailHookEvent for heritage

--- Event/UserMailHookEvent.php
+++ Event/UserMailHookEvent.php
@@ -5,7 +5,7 @@ namespace Swm\Bundle\MailHookBundle\Event;
 use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserMailHookEvent extends Event implements HookEventInterface
+class UserMailHookEvent extends MailHookEvent implements HookEventInterface
 {
     /**
      * @var string
@@ -27,7 +27,7 @@ class UserMailHookEvent extends Event implements HookEventInterface
      * @param UserInterface $user
      * @param array         $metaData
      */
-    public function __construct($name  = null, UserInterface $user  = null, array $metaData = array())
+    public function __construct(UserInterface $user  = null, $name  = null, array $metaData = array())
     {
         $this->name     = $name;
         $this->user     = $user;
@@ -54,54 +54,6 @@ class UserMailHookEvent extends Event implements HookEventInterface
     public function setUser(UserInterface $user)
     {
         $this->user = $user;
-
-        return $this;
-    }
-
-    /**
-     * Gets the value of name.
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Sets the value of name.
-     *
-     * @param string $name the name
-     *
-     * @return this
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Gets the value of metaData.
-     *
-     * @return array
-     */
-    public function getMetaData()
-    {
-        return $this->metaData;
-    }
-
-    /**
-     * Sets the value of metaData.
-     *
-     * @param array $metaData the meta data
-     *
-     * @return this
-     */
-    public function setMetaData(array $metaData = array())
-    {
-        $this->metaData = $metaData;
 
         return $this;
     }

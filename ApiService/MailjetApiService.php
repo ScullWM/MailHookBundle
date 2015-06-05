552923d67af498e78c29ba0f6deb7043932a56d1
Thomas Perez <thomas@scullwm.com>
2015-06-05 22:31:47 +0200

    fix mailjet api service

--- ApiService/MailjetApiService.php
+++ ApiService/MailjetApiService.php
@@ -8,15 +8,15 @@ use Swm\Bundle\MailHookBundle\SwmMailHookEvent;
 class MailjetApiService extends BaseApiService
 {
     private $eventAssoc = array(
-        'send'        => SwmMailHookEvent::MAILHOOK_SEND,
-        'deferral'    => SwmMailHookEvent::MAILHOOK_DEFERRAL,
-        'open'        => SwmMailHookEvent::MAILHOOK_OPEN,
-        'click'       => SwmMailHookEvent::MAILHOOK_CLICK,
-        'bounce'      => SwmMailHookEvent::MAILHOOK_SOFTBOUNCE,
-        'blocked'     => SwmMailHookEvent::MAILHOOK_HARDBOUNCE,
-        'spam'        => SwmMailHookEvent::MAILHOOK_SPAM,
-        'unsub'       => SwmMailHookEvent::MAILHOOK_UNSUB,
-        'typofix'     => SwmMailHookEvent::MAILHOOK_OTHER,
+        'send'     => SwmMailHookEvent::MAILHOOK_SEND,
+        'deferral' => SwmMailHookEvent::MAILHOOK_DEFERRAL,
+        'open'     => SwmMailHookEvent::MAILHOOK_OPEN,
+        'click'    => SwmMailHookEvent::MAILHOOK_CLICK,
+        'bounce'   => SwmMailHookEvent::MAILHOOK_SOFTBOUNCE,
+        'blocked'  => SwmMailHookEvent::MAILHOOK_HARDBOUNCE,
+        'spam'     => SwmMailHookEvent::MAILHOOK_SPAM,
+        'unsub'    => SwmMailHookEvent::MAILHOOK_UNSUB,
+        'typofix'  => SwmMailHookEvent::MAILHOOK_OTHER,
     );
 
     /**
@@ -24,13 +24,11 @@ class MailjetApiService extends BaseApiService
      */
     public function bind()
     {
-        if (0 === strpos($this->request->headers->get('Content-Type'), 'application/json')) {
-            $metaData = json_decode($this->request->getContent(), true);
-        }
+        $metaData = json_decode($this->request->getContent(), true);
 
-        $email = $hook['email'];
-        $event = $hook['event'];
+        $email = $metaData['email'];
+        $event = $metaData['event'];
 
-        return array(new DefaultHook($event, $email, 'mailjet', $metaData));
+        return array(new DefaultHook($event, $email, 'mailjet', $metaData, $this->eventAssoc[$event]));
     }
 }

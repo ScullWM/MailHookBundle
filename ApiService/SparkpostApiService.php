999256b5113d0bd8eb2e2a46632a6e80b8ba793f
Thomas P <ScullWM@users.noreply.github.com>
2016-04-23 18:31:14 +0200

    Sparkpost support
    * Wip add sparkpost events
    * Complete Sparkpost event association

---
+++ ApiService/SparkpostApiService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Swm\Bundle\MailHookBundle\ApiService;
+
+use Swm\Bundle\MailHookBundle\Hook\DefaultHook;
+use Swm\Bundle\MailHookBundle\SwmMailHookEvent;
+
+class SparkpostApiService extends BaseApiService
+{
+    private $eventAssoc = array(
+        'open'             => SwmMailHookEvent::MAILHOOK_OPEN,
+        'click'            => SwmMailHookEvent::MAILHOOK_CLICK,
+        'bounce'           => SwmMailHookEvent::MAILHOOK_HARDBOUNCE,
+        'spam_complaint'   => SwmMailHookEvent::MAILHOOK_SPAM,
+        'list_unsubscribe' => SwmMailHookEvent::MAILHOOK_UNSUB,
+        'link_unsubscribe' => SwmMailHookEvent::MAILHOOK_UNSUB,
+        'delay'            => SwmMailHookEvent::MAILHOOK_DEFERRAL,
+        'delivery'         => SwmMailHookEvent::MAILHOOK_SEND,
+        'policy_rejection' => SwmMailHookEvent::MAILHOOK_REJECT,
+        'out_of_band'      => SwmMailHookEvent::MAILHOOK_HARDBOUNCE,
+    );
+
+    /**
+     * @param  array  $hook
+     * @return HookInterface
+     */
+    private function bindHook(array $hook)
+    {
+        $email = $hook['msys']['rcpt_to'];
+        $event = $hook['msys']['message_event']['type'];
+        return new DefaultHook($event, $email, 'mandrill', $hook, $this->eventAssoc[$event]);
+    }
+
+    /**
+     * @return array<HookInterface>
+     */
+    public function bind()
+    {
+        if (!$this->request->get('results')) {
+            throw new \Exception("Could not find data");
+        }
+
+        $sparkpostEvents = json_decode($this->request->get('results'), true);
+
+        return array_map([$this, 'bindHook'], $sparkpostEvents);
+    }
+}
